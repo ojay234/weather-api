8c755936996a43ecac2b224bf3961c77ca87e4c4
ojay234 <davidace998@gmail.com>
2023-01-24 16:56:11 +0100

    updates

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,23 @@
-const request = require("request");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=41df0b88e0a5ccccb602bc4987d9ceb8&query=37.8267,-122.4233";
+const address = process.argv[2];
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
-});
+if (!address) {
+  console.log("please provide an address");
+} else {
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
+}
